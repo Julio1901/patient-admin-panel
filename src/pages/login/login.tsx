e6d6e982d2e037c63321e61df2e42c91e1cff1e6
Julio Cesar <js.cesar42@gmail.com>
2024-01-20 16:32:29 -0300

    feat: Image login added ininto screen

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,24 @@
 
-import { MainContainer } from "./styles"
+import { MainContainer, LoginFormContainer, Title, SubTitle, InputTitle, InputText, PasswordItensContainer, TextButton, LoginImage } from "./styles"
 
-
+import LoginImageSrc  from '../../assets/images/doctor-login-image.jpg'
 
 export const  Login = () => {
     return(
        <MainContainer>
-
+            <LoginFormContainer>
+                <Title>Welcome back!</Title>
+                <SubTitle>Enter your Credentials to access your account</SubTitle>
+                <InputTitle>Email address</InputTitle>
+                <InputText placeholder="Enter your email"/>
+                <PasswordItensContainer>
+                    <InputTitle style={{marginTop:20}}>Password</InputTitle>
+                    <TextButton style={{marginTop:20}}>Forgot password</TextButton>
+                </PasswordItensContainer>
+                <InputText placeholder="Enter your password"/>
+            </LoginFormContainer>
+            <LoginImage src={LoginImageSrc}/>
+ 
        </MainContainer>
     )
 
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -3,5 +3,79 @@ import styled from "styled-components";
 export const MainContainer = styled.div`
     width: 100%;
     height: 100vh;
-    background-color: aquamarine;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`
+
+export const LoginFormContainer = styled.div`
+    width: 404px;
+    height: 438px;
+    display: flex;
+    flex-direction: column;
+    background: rgb(18, 128, 255, 0.2);
+    border-radius: 30px;
+    margin-left: 15px;
+    padding-left: 10px;
+    padding-right: 10px;
+    justify-content: center;
+`
+
+export const Title = styled.text`
+   @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,700;0,800;0,900;1,700;1,800;1,900&display=swap');
+    font-size: 32px;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-style: normal;
+    padding-top: 5px;
+    padding-bottom: 5px;
+`
+
+
+export const SubTitle = styled.text`
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,700;0,800;0,900;1,700;1,800;1,900&display=swap');
+    font-size: 16px;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-style: normal;
+`
+
+
+export const InputTitle = styled.text`
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,700;0,800;0,900;1,700;1,800;1,900&display=swap');
+    font-size: 14px;
+    font-family: 'Poppins', sans-serif;
+    margin-top: 60px;
+`
+
+export const InputText = styled.input`
+    width: 100%;
+    height: 32px;
+    border: 1px solid #D9D9D9;
+    border-radius: 10px;
+    box-sizing: border-box;
+`
+
+export const PasswordItensContainer = styled.div`
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: row;
+`
+
+export const TextButton = styled.div`
+     color: #0C2A92;
+    cursor: pointer;
+    font-weight: bold;
+    margin-left: auto;
+    padding-right: 5px;
+`
+
+export const LoginImage = styled.img`
+    width: 780px;
+    height: 70%;
+    border-top-left-radius: 30px; 
+    border-bottom-left-radius: 30px; 
+    margin-left: auto; 
+
 `
