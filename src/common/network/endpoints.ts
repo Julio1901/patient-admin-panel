2f2d88bc555cb8a3c65d3dc3786a7d29b41870de
Julio Cesar <js.cesar42@gmail.com>
2024-01-29 23:12:06 -0300

    feat: network implemented

---
+++ src/common/network/endpoints.ts
@@ -0,0 +1,3 @@
+const BASE_URL = 'https://my-json-server.typicode.com/Julio1901/JsonMockResources' 
+
+export const PATIENTS_ENDPOIN: string = `${BASE_URL}/patients`
--- src/pages/mainPanel/main-panel.tsx
+++ src/pages/mainPanel/main-panel.tsx
@@ -1,6 +1,33 @@
 
+import axios from 'axios';
+import { PATIENTS_ENDPOIN } from '../../common/network/endpoints';
+import { IPatient } from '../../common/network/interfaces';
+import { useEffect, useRef } from 'react';
+
 
 export const MainPanel = () => {
+    
+    const initialized = useRef(false)
+
+    useEffect( () => {
+        if (!initialized.current) {
+            initialized.current = true
+            fetchData()
+            console.log('chamou')
+        }
+    }, [])
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get<IPatient[]>(PATIENTS_ENDPOIN)
+            console.log(response)
+        }catch (error: any) {
+            alert(`Error fetching data: ${(error as Error).message ?? 'Unknown error'}`);
+          }
+
+    }
+
+
 
     return(
         <div>
